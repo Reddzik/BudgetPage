0b2c489d12aca6d3ff8be898758e3a30f7bc5b6c
Reddzik <mateusz.redzik@o2.pl>
2020-11-30 20:36:09 +0100

    created common reducer + configured store

---
+++ src/data/reducers/common.reducer.js
@@ -0,0 +1,41 @@
+import {
+  CATEGORIES_GET,
+  CATEGORIES_GET_REQUEST,
+  CATEGORIES_GET_FAILURE,
+  CATEGORIES_GET_SUCCESS,
+  LOADING_STATES,
+} from "data/constants";
+
+const initialState = {
+  loadingState: {},
+  allCategories: [],
+};
+
+function common(state = initialState, action) {
+  const newloadingState = { ...state.loadingState };
+  switch (action.type) {
+    case CATEGORIES_GET_REQUEST:
+      return {
+        ...state,
+        loadingState: {
+          ...state.loadingState,
+          [action.type]: true,
+        },
+      };
+    case CATEGORIES_GET_SUCCESS:
+      delete newloadingState.CATEGORIES_GET_REQUEST;
+      return {
+        ...state,
+        loadingState: newloadingState,
+        allCategories: action.payload,
+      };
+    case CATEGORIES_GET_FAILURE:
+      delete newloadingState.CATEGORIES_GET_REQUEST;
+      return {
+        loadingState: newloadingState,
+        allCategories: [],
+      };
+    default:
+      return state;
+  }
+}
