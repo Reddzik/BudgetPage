16cb0a29151c85ddbdb8e070157b1d985b07764b
Mateusz <54061275+Reddzik@users.noreply.github.com>
2020-11-30 15:44:30 +0100

    initializing redux and store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import RootApp from "./App";
 import "./i18n/i18n";
+
+import configureStore from "data/store";
 import reportWebVitals from "./reportWebVitals";
 
+const store = configureStore();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <RootApp />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RootApp />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
